2b79766a66c7ea019b39d6de0cee497b56e02bf5
LimeStreem <LimeStreem@gmail.com>
2016-10-16 12:43:07 +0900

    Initial project generation

---
+++ README.md
@@ -0,0 +1,15 @@
+# webvr
+> The web vr extension for grimoire.js
+
+Describe about this package here.
+(パッケージに関する説明をここに記述する。)
+
+# installation
+
+```bash
+  npm install grimoirejs-webvr --save
+```
+
+# LICENSE
+
+MIT
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+// Please do not change the name of variable on the line below.
+import GrimoireInterface from "grimoirejs";
+// IMPORTS would be replaced for importing components.
+//<%=IMPORTS%>
+
+GrimoireInterface.register(async () => {
+  // REGISTER would be replaced to actual codes to register components.
+  //<%=REGISTER%>
+
+  // You can edit code here.
+
+
+
+
+});
