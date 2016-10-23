acb5e67b6d7f3c8bf9fd9f87eb355367884fc1da
case2912 <shiroyama@maia.eonet.ne.jp>
2016-10-23 16:09:41 +0900

    fix: component

--- src/Components/GyroCameraComponent.ts
+++ src/Components/GyroCameraComponent.ts
@@ -4,24 +4,43 @@ import IAttributeDeclaration from "grimoirejs/lib/Node/IAttributeDeclaration";
 import {Quaternion} from "grimoirejs-math";
 
 export default class GyroCameraComponent extends Component {
-  public static attributes: { [key: string]: IAttributeDeclaration } = {
-    baseRotation: {
-      converter: "Rotation3",
-      defaultValue: "z(90d)"
+    public static attributes: { [key: string]: IAttributeDeclaration } = {
+        baseRotation: {
+            converter: "Rotation3",
+            defaultValue: "z(90d)"
+        },
+        config: {
+            converter: "String",
+            defaultValue: "+Y+X-Z"
+        }
+    };
+
+    private _transform: TransformComponent;
+
+    private _baseRotation: Quaternion;
+
+    private _config: string;
+
+    public $mount(): void {
+        this.getAttribute("baseRotation").boundTo("_baseRotation");
+        window.addEventListener("deviceorientation", this._handleOrientation.bind(this), true);
+        this._transform = this.node.getComponent("Transform") as TransformComponent;
+        this.getAttribute("config").boundTo("_config");
     }
-  };
 
-  private _transform: TransformComponent;
-
-  private _baseRotation: Quaternion;
-
-  public $mount(): void {
-    this.getAttribute("baseRotation").boundTo("_baseRotation");
-    window.addEventListener("deviceorientation", this._handleOrientation.bind(this), true);
-    this._transform = this.node.getComponent("Transform") as TransformComponent;
-  }
-
-  private _handleOrientation(e: DeviceOrientationEvent): void {
-    this._transform.localRotation = Quaternion.multiply(this._baseRotation, Quaternion.euler(e.beta / 180 * Math.PI, e.alpha / 180 * Math.PI, -e.gamma / 180 * Math.PI, ));
-  }
+    private _handleOrientation(e: DeviceOrientationEvent): void {
+        const value: number[] = [e.alpha, e.beta, e.gamma];
+        const array: number[] = [1, 1, 1];
+        if (this.node.enabled) {
+            for (let i = 0; i < 3; i++) {
+                array[i] *= this._config.charAt(2 * i) == "+" ? 1 : -1;
+                if (this._config.charAt(2 * i + 1) == "X") {
+                    array[1] *= value[i];
+                } else if (this._config.charAt(2 * i + 1) == "Y") {
+                    array[2] *= value[i];
+                } else { array[3] *= value[i]; }
+            }
+            this._transform.localRotation = Quaternion.multiply(this._baseRotation, Quaternion.euler(array[0] / 180 * Math.PI, array[1] / 180 * Math.PI, array[2] / 180 * Math.PI));
+        }
+    }
 }
